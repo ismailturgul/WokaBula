9d5bb1255c6a538046b7be8409f56b7fe4276335
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-11-21 05:21:59 +0100

    Added Movement

--- Woka Bula/Assets/Script/Player_Movement.cs
+++ Woka Bula/Assets/Script/Player_Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Controller : MonoBehaviour
+public class Player_Movement : MonoBehaviour
 {
     public float move_Speed;
 
@@ -11,7 +11,7 @@ public class Player_Controller : MonoBehaviour
 
     void Update()
     {
-        if(!is_Moving)
+        if (!is_Moving)
         {
             input.x = Input.GetAxisRaw("Horizontal");   // + or - value when pressed
             input.y = Input.GetAxisRaw("Vertical");     // + or - value when pressed
@@ -19,8 +19,8 @@ public class Player_Controller : MonoBehaviour
             if (input != Vector2.zero)
             {
                 var target_Pos = transform.position;
-                target_Pos.x = input.x;
-                target_Pos.y = input.y;
+                target_Pos.x += input.x;
+                target_Pos.y += input.y;
 
                 StartCoroutine(Move(target_Pos));
             }
@@ -29,13 +29,14 @@ public class Player_Controller : MonoBehaviour
     //chorotine
     IEnumerator Move(Vector3 target_Pos)
     {
-        while ((target_Pos - transform.position).sqrMagnitude > Mathf.Epsilon) {
-        
+        while ((target_Pos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+
             transform.position = Vector3.MoveTowards(transform.position, target_Pos, move_Speed * Time.deltaTime);
             yield return null;
         }
         transform.position = target_Pos;
 
-        is_Moving= false; 
+        is_Moving = false;
     }
 }
