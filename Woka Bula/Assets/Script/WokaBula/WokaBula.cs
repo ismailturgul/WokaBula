c79aa5e9fe34fc2103726728f4aacb46508456d0
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-11-23 21:49:24 +0100

    Added Properties function based on Level

--- Woka Bula/Assets/Script/WokaBula/WokaBula.cs
+++ Woka Bula/Assets/Script/WokaBula/WokaBula.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WokaBula : MonoBehaviour
+public class WokaBula
 {
-    // Start is called before the first frame update
-    void Start()
+    WokaBula_Base _base;
+    int level;
+
+    public WokaBula(WokaBula_Base pbase, int plevel)
     {
-        
+        _base = pbase;
+        level = plevel;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public int Attack
     {
-        
+        get { return Mathf.FloorToInt((_base.Attack * level) / 100f) + 5; }
+    }
+    public int Defense
+    {
+        get { return Mathf.FloorToInt((_base.Defense * level) / 100f) + 5; }
+    }
+    public int Sp_Attack
+    {
+        get { return Mathf.FloorToInt((_base.Sp_Attack * level) / 100f) + 5; }
+    }
+    public int Sp_Defense
+    {
+        get { return Mathf.FloorToInt((_base.Sp_Defense * level) / 100f) + 5; }
+    }
+    public int Speed
+    {
+        get { return Mathf.FloorToInt((_base.Speed * level) / 100f) + 5; }
+    }
+    public int Max_Hp
+    {
+        get { return Mathf.FloorToInt((_base.Max_Hp * level) / 100f) + 10; }
     }
 }
