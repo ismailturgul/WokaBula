a3491a0be5658b9e8c955b9ca6c2c89f24545f83
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-11-23 20:50:08 +0100

    Added a WokaBula scriptable Object
    And the Game,Resources & Wokabulas Folder

--- Woka Bula/Assets/Script/WokaBula/WokaBula_Base.cs
+++ Woka Bula/Assets/Script/WokaBula/WokaBula_Base.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WokaBula_Base : MonoBehaviour
+[CreateAssetMenu(fileName = "WokaBula", menuName = "WokaBula/Create new wokaBula")]
+public class WokaBula_Base : ScriptableObject
 {
 
     [SerializeField] string name;
