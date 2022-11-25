b4ab5290d0ff4f367cde355176a6bb8f16690445
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-11-25 18:31:18 +0100

    Health Bar Script
    Added Health Bar script

---
+++ Woka Bula/Assets/Script/Battle/Hp_Bar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hp_Bar : MonoBehaviour
+{
+    [SerializeField] GameObject health;
+
+
+    private void Start()
+    {
+        health.transform.localScale = new Vector3(0, 5f, 1);
+    }
+}
