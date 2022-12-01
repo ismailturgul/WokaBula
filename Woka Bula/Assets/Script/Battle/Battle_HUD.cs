b30a6e31d9addcd3385b51dbbe4091091e904118
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2022-12-01 21:32:35 +0100

    Hp script & Battle HUD have no bugs anymore

--- Woka Bula/Assets/Script/Battle/Battle_HUD.cs
+++ Woka Bula/Assets/Script/Battle/Battle_HUD.cs
@@ -7,13 +7,14 @@ public class Battle_HUD : MonoBehaviour
 
     [SerializeField] Text name_Text;
     [SerializeField] Text level_Text;
-    [SerializeField] Text hp_Bar;
+    [SerializeField] Hp_Bar hp_Bar;
 
     public void SetData(WokaBula wokabula)
     {
         name_Text.text = wokabula.Base.Name;
         level_Text.text = "Lvl" + wokabula.Level;
-        hp_Bar.SetHP((float) wokabula.HP / wokabula.Max_Hp); 
+        hp_Bar.Set_HP((float)wokabula.HP / wokabula.Max_Hp);
+
 
     }
 }
--- Woka Bula/Assets/Script/Battle/Hp_Bar.cs
+++ Woka Bula/Assets/Script/Battle/Hp_Bar.cs
@@ -7,7 +7,7 @@ public class Hp_Bar : MonoBehaviour
     [SerializeField] GameObject health;
 
 
-    public void SetHP(float hp_Normalized)
+    public void Set_HP(float hp_Normalized)
     {
         health.transform.localScale = new Vector3(hp_Normalized, 1);
     }
